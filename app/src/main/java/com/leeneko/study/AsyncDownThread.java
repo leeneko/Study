dc4a471faaed8379e77bec4b555516c83e761ed0
leeneko <nexxonc5@gmail.com>
2019-01-25 17:29:42 +0900

    initial commit

---
+++ app/src/main/java/com/leeneko/study/AsyncDownThread.java
@@ -0,0 +1,60 @@
+package com.leeneko.study;
+
+import android.os.Handler;
+import android.os.Message;
+import android.os.NetworkOnMainThreadException;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class AsyncDownThread extends Thread {
+    String mAddr;
+    Handler mHandler;
+
+    AsyncDownThread(String addr, Handler handler) {
+        mAddr = addr;
+        mHandler = handler;
+    }
+
+    public void run() {
+        String result = downloadHtml(mAddr);
+
+        Message message = mHandler.obtainMessage();
+        message.obj = result;
+        mHandler.sendMessage(message);
+    }
+
+    private String downloadHtml(String addr) {
+        StringBuilder html = new StringBuilder();
+        try {
+            URL url = new URL(addr);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            if (conn != null) {
+                conn.setConnectTimeout(10000);
+                conn.setUseCaches(false);
+                if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    BufferedReader br = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream()));
+                    while (true) {
+                        String line = br.readLine();
+                        if (line == null) break;
+                        html.append(line);
+                    }
+                    br.close();
+                }
+                conn.disconnect();
+            }
+        } catch (NetworkOnMainThreadException e) {
+            return "Error : NetworkOnMainThreadException " + e.getMessage();
+        } catch (Exception e) {
+            return "Error : " + e.getMessage();
+        }
+
+        return html.toString();
+    }
+
+}
--- app/src/main/java/com/leeneko/study/HomeActivity.java
+++ app/src/main/java/com/leeneko/study/HomeActivity.java
@@ -9,21 +9,30 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Map<String, Class<?>> btnActs = new LinkedHashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        btnActs.put("Main", MainActivity.class);
+        btnActs.put("Image", ImageActivity.class);
+        btnActs.put("Intent", SubActivity.class);
+        btnActs.put("ListView", ListActivity.class);
+        btnActs.put("Custom ListView", ChatActivity.class);
+
         final List<String> arData = new ArrayList<>();
-        arData.add("Main");
-        arData.add("Image");
-        arData.add("Intent");
-        arData.add("ListView");
-        arData.add("Custom ListView");
+        for (String key: btnActs.keySet()) {
+            arData.add(key);
+        }
 
         final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arData);
         ListView lv = findViewById(R.id.lvMenu);
@@ -31,12 +40,9 @@ public class HomeActivity extends AppCompatActivity {
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = null;
-                if (position == 0) { intent = new Intent(HomeActivity.this, MainActivity.class); }
-                if (position == 1) { intent = new Intent(HomeActivity.this, ImageActivity.class); }
-                if (position == 2) { intent = new Intent(HomeActivity.this, SubActivity.class); }
-                if (position == 3) { intent = new Intent(HomeActivity.this, ListActivity.class); }
-                if (position == 4) { intent = new Intent(HomeActivity.this, ChatActivity.class); }
+                ListView lv = (ListView) parent;
+                String item = (String) lv.getItemAtPosition(position);
+                Intent intent = new Intent(HomeActivity.this, btnActs.get(item));
                 startActivity(intent);
             }
         });
